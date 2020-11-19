82bc92f91e25ca299d560feee1e889d0a4422782
Zuhrul Anam <48517203+zuhrulinc@users.noreply.github.com>
2020-11-19 16:10:22 +0700

    Add files via upload
    s

--- testlogin.py
+++ testlogin.py
@@ -10,7 +10,7 @@ import random
 
 BASE_URL = 'https://www.instagram.com/'
 LOGIN_URL = BASE_URL + 'accounts/login/ajax/'
-USER_AGENT = None
+USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0"
 proxyAddress = None
 
 #Setting some headers and refers
@@ -18,5 +18,6 @@ session = requests.Session()
 session.headers = {'user-agent': USER_AGENT}
 session.headers.update({'Referer': BASE_URL})
 
+req = session.get(BASE_URL)
 req = session.get(BASE_URL)
 print(req.cookies)
