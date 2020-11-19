93594f9e62d91809bf41ea172e83a5b7613f26b7
Zuhrul Anam <48517203+zuhrulinc@users.noreply.github.com>
2020-11-19 16:02:13 +0700

    Add files via upload

--- testlogin.py
+++ testlogin.py
@@ -1,10 +1,15 @@
 #test login
 #Creating URL, usr/pass and user agent variables
 
+import requests
+import json
+import sys
+import time
+import random
+
+
 BASE_URL = 'https://www.instagram.com/'
 LOGIN_URL = BASE_URL + 'accounts/login/ajax/'
-USERNAME = 'zuhrul.inc@gmail.com'
-PASSWD = '#PWD_INSTAGRAM_BROWSER:10:1597642290:AUJQACdOp//rgXKjiWTxcK3xQu64UXrWlhRawK1WmpilnmV8nKZnZiwR2DoezokWLwilEAsN8+QccvMNAq50+XJoyFsILG/FoIsERIW36oqEyjq+3tPoSiNpp2tRZG+LOoW5TNf1cZXTd6s//2Y='
 USER_AGENT = None
 proxyAddress = None
 
